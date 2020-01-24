7251fa02004bc26e8d1258f264de69987da82240
Nirav Kukadiya <nirav.kukadiya89@gmail.com>
2020-01-24 13:25:31 +0530

    Datatypes examples

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,2 @@
 print("Hello World")
-print("*" * 10)
+print("*" * 20)
--- primitiveTypes.py
+++ primitiveTypes.py
@@ -1,20 +1,72 @@
 # Variables
 
 Count = 1000
-print(Count)
+print("variable :",Count)
 
 # DataTypes / Primitive Types
 
 # Text Type:	str
 
+x = "Hello World"
+print("str :", x)
+
 # Numeric Types:	int, float, complex
+a = 10
+print("int :",a)
+b=1.4
+print("float :",b)
+c=5j
+print("complex :",c)
 
 # Sequence Types:	list, tuple, range
 
+#list
+
+d = ["Nirav","P","Kukadiya"]
+print("List :",d)
+print("type of d",type(d)) 
+
+#tuple
+
+e = ("tuple","example")
+print("tuple", e)
+
+#range
+
+f = range(2,100)
+print("range",f)
+
 # Mapping Type:	dict
+
+g={"Name":"John","Age":"30"}
+print("dict :",g)
 
 # Set Types:	set, frozenset
 
+#set
+
+h={"Cycle","byk","car"}
+print("set : ", h)
+
+#frozenset
+
+i = frozenset({"google","Yahoo","Bing"})
+print("Frozenset :",i)
+
 # Boolean Type:	bool
 
+j = True
+print("boolean :",j)
+
 # Binary Types:	bytes, bytearray, memoryview
+
+k=b"Hello World"
+print("bytes :",k)
+
+#bytearray
+L= bytearray(5)
+print("bytearray : ", L)
+
+#memoryView
+M= memoryview(bytes(5))
+print("memoryview : ", M)
