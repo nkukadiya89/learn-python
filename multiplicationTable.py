b68586e9a7cd9a9871bb185384e2a0e2357e24ac
Nirav Kukadiya <nirav.kukadiya89@gmail.com>
2020-01-26 10:28:19 +0530

    Multiplication table

---
+++ multiplicationTable.py
@@ -0,0 +1,9 @@
+# Multiplication table 
+
+#Take input from user
+num = int(input("Display multiplication table of :"))
+
+#Interate 10 times from i = 1 to 10
+
+for i in range(1,11):
+    print(num, 'x', i, '=',num*i)
